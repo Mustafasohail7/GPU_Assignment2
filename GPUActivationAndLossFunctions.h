af7098c235cba672fb6f31198a72610fbc0f5ae9
Mustafasohail7 <mustufasohail7@gmail.com>
2024-03-11 10:22:18 +0500

    changed functions inside activation for GPU

--- GPUActivationAndLossFunctions.h
+++ GPUActivationAndLossFunctions.h
@@ -4,10 +4,10 @@
 #include <cmath>
 #include <Eigen/Dense>
 
-extern Eigen::MatrixXf cudaMatrixMul(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
-extern Eigen::MatrixXf cudaMatrixScalarMul(const Eigen::MatrixXf &M, float scalar);
-extern Eigen::MatrixXf cudaMatrixAdd(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
-extern Eigen::MatrixXf cudaMatrixSub(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
+extern Eigen::MatrixXf HostMatrixMultiplication(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
+extern Eigen::MatrixXf HostMatrixScalarMultiplication(const Eigen::MatrixXf &M, float scalar);
+extern Eigen::MatrixXf HostMatrixAddition(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
+extern Eigen::MatrixXf HostMatrixSubtraction(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
 
 //activation functions
 float sigmoid(float x)
@@ -47,20 +47,20 @@ float one_minus(float x)
 float mse(Eigen::MatrixXf& y_true, Eigen::MatrixXf& y_pred)
 {
   // Calculate on GPU
-	return cudaMatrixMul(cudaMatrixSub(y_true, y_pred), cudaMatrixSub(y_true, y_pred).transpose()).mean();
+	return HostMatrixMultiplication(HostMatrixSubtraction(y_true, y_pred), HostMatrixSubtraction(y_true, y_pred).transpose()).mean();
 }
 
 Eigen::MatrixXf mse_prime(Eigen::MatrixXf& y_true, Eigen::MatrixXf& y_pred)
 {
   // Calculate on GPU
-  return cudaMatrixScalarMul(cudaMatrixScalarMul(cudaMatrixSub(y_pred, y_true), 2), 1 / (y_true.rows() * y_true.cols()));
+  return HostMatrixScalarMultiplication(HostMatrixScalarMultiplication(HostMatrixSubtraction(y_pred, y_true), 2), 1 / (y_true.rows() * y_true.cols()));
 	// return  2 * (y_pred - y_true) / (y_true.rows()*y_true.cols());
 }
 
 float binary_cross_entropy(Eigen::MatrixXf& y_true, Eigen::MatrixXf& y_pred)
 {
   // Calculate on GPU
-  return cudaMatrixMul(cudaMatrixSub(cudaMatrixScalarMul(y_true, -1), cudaMatrixMul(y_pred, cudaMatrixScalarMul(y_true, -1)).log()), cudaMatrixSub(cudaMatrixScalarMul(cudaMatrixScalarMul(y_true, -1), cudaMatrixMul(y_pred, cudaMatrixScalarMul(y_true, -1)).log()), cudaMatrixScalarMul(cudaMatrixScalarMul(y_true, -1), cudaMatrixMul(y_pred, cudaMatrixScalarMul(y_true, -1)).log())).transpose()).mean();
+  return HostMatrixMultiplication(HostMatrixSubtraction(HostMatrixScalarMultiplication(y_true, -1), HostMatrixMultiplication(y_pred, HostMatrixScalarMultiplication(y_true, -1)).log()), HostMatrixSubtraction(HostMatrixScalarMultiplication(HostMatrixScalarMultiplication(y_true, -1), HostMatrixMultiplication(y_pred, HostMatrixScalarMultiplication(y_true, -1)).log()), HostMatrixScalarMultiplication(HostMatrixScalarMultiplication(y_true, -1), HostMatrixMultiplication(y_pred, HostMatrixScalarMultiplication(y_true, -1)).log())).transpose()).mean();
 }
 
 #endif
