9e8b282cc6a5ada39a4c9e5ff2fa7427f3ac38bc
Mustafasohail7 <mustufasohail7@gmail.com>
2024-03-11 12:37:53 +0500

    file name modified

--- GPUNeuralNetwork.h
+++ GPUNeuralNetwork.h
@@ -1,196 +1,56 @@
-#ifndef NEURAL_NET_INC
-#define NEURAL_NET_INC
-#pragma once
+#include <iostream>
+#include <vector>
+#include "GpuNeuralNetwork.h"
+#include "ActivationAndLossFunctions.h"
 
 #include <iostream>
 #include <vector>
-#include <Eigen/Dense>
-#include <numeric> //std::iota
+#include <chrono>
+#include <fstream>
+#include "GpuNeuralNetwork.h"
+#include "ActivationAndLossFunctions.h"
 
-extern Eigen::MatrixXf HostMatrixMultiply(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
-extern Eigen::MatrixXf HostMatrixScalarMultiply(const Eigen::MatrixXf &M, float scalar);
-extern Eigen::MatrixXf HostMatrixAddition(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
-extern Eigen::MatrixXf HostMatrixSubtraction(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
-
-void printMatrixSize(const std::string msg, const Eigen::MatrixXf &m)
+void timeAndStoreOperations(const std::string& filePath)
 {
-  std::cout << msg.c_str() << "[" << m.rows() << "," << m.cols() << "]" << std::endl;
+	std::ofstream outputFile(filePath, std::ios::app);
+
+	std::cout << "Using Eigen ver: " << EIGEN_WORLD_VERSION << "." << EIGEN_MAJOR_VERSION << "." << EIGEN_MINOR_VERSION << std::endl;
+
+	// test the XOR solver
+	Eigen::MatrixXf x_train{{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+	Eigen::MatrixXf y_train{{0}, {1}, {1}, {0}};
+
+	GPUNetwork nn;
+	nn.add(new GPUDenseLayer(2, 3));
+	nn.add(new GPUActivationLayer(tanh2, tanh_prime));
+	nn.add(new GPUDenseLayer(3, 1));
+	nn.add(new GPUActivationLayer(tanh2, tanh_prime));
+
+	nn.use(mse, mse_prime);
+
+	// Measure execution time
+	auto startTime = std::chrono::high_resolution_clock::now();
+
+	// train
+	nn.fit(x_train, y_train, 1000, 0.1f);
+
+	// test
+	std::vector<Eigen::MatrixXf> output = nn.predict(x_train);
+	for (Eigen::MatrixXf out : output)
+		std::cout << out << std::endl;
+
+	// End timing
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+
+	outputFile << duration << std::endl;
+
+	outputFile.close();
 }
 
-class GPULayer
+int main()
 {
-public:
-  GPULayer() : input(), output() {}
-  virtual ~GPULayer() {}
-
-  virtual Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf &input) = 0;
-  virtual Eigen::MatrixXf backwardPropagation(Eigen::MatrixXf &output, float learningRate) = 0;
-
-protected:
-  Eigen::MatrixXf input;
-  Eigen::MatrixXf output;
-};
-
-class GPUDenseLayer : public GPULayer
-{
-public:
-  GPUDenseLayer(int inputSize, int outputSize)
-  {
-    weights = HostMatrixScalarMultiply(Eigen::MatrixXf::Random(inputSize, outputSize), 0.5f);
-    bias = HostMatrixScalarMultiply(Eigen::MatrixXf::Random(1, outputSize), 0.5f);
-  }
-
-  Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf &input)
-  {
-    this->input = input;
-    this->output = HostMatrixAddition(HostMatrixMultiply(input, weights), bias);
-    return this->output;
-  }
-
-  
-  Eigen::MatrixXf backwardPropagation(Eigen::MatrixXf &outputError, float learningRate)
-  {
-    Eigen::MatrixXf inputError = HostMatrixMultiply(outputError, weights.transpose());
-    Eigen::MatrixXf weightsError = HostMatrixMultiply(input.transpose(), outputError);
-
-    weights = HostMatrixSubtraction(weights, HostMatrixScalarMultiply(weightsError, learningRate));
-    bias = HostMatrixSubtraction(bias, HostMatrixScalarMultiply(outputError, learningRate));
-
-    return inputError;
-  }
-
-private:
-  Eigen::MatrixXf weights;
-  Eigen::MatrixXf bias;
-};
-
-class GPUActivationLayer : public GPULayer
-{
-public:
-  GPUActivationLayer(std::function<float(float)> activation,
-                     std::function<float(float)> activationPrime)
-  {
-    this->activation = activation;
-    this->activationPrime = activationPrime;
-  }
-
-  Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf &input)
-  {
-    this->input = input;
-    this->output = input.unaryExpr(activation);
-    return this->output;
-  }
-
-  Eigen::MatrixXf backwardPropagation(Eigen::MatrixXf &outputError, float learningRate)
-  {
-    return (input.unaryExpr(activationPrime).array() * outputError.array()).matrix();
-  }
-
-private:
-  std::function<float(float)> activation;
-  std::function<float(float)> activationPrime;
-};
-
-class GPUFlattenLayer : public GPULayer
-{
-public:
-  Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf &input)
-  {
-    this->input = input;
-    this->output = input;
-    this->output.resize(1, input.rows() * input.cols()); // flatten
-    return this->output;
-  }
-  Eigen::MatrixXf backwardPropagation(Eigen::MatrixXf &outputError, float learningRate)
-  {
-    outputError.resize(input.rows(), input.cols());
-    return outputError;
-  }
-};
-
-class GPUNetwork
-{
-public:
-  GPUNetwork() {}
-  virtual ~GPUNetwork() {}
-
-  void add(GPULayer *layer)
-  {
-    layers.push_back(layer);
-  }
-
-  void use(std::function<float(Eigen::MatrixXf &, Eigen::MatrixXf &)> lossF, std::function<Eigen::MatrixXf(Eigen::MatrixXf &, Eigen::MatrixXf &)> lossDer)
-  {
-    loss = lossF;
-    lossPrime = lossDer;
-  }
-
-  std::vector<Eigen::MatrixXf> predict(Eigen::MatrixXf input)
-  {
-    int samples = input.rows();
-
-    std::vector<Eigen::MatrixXf> result;
-
-    // forward propagation
-    for (int j = 0; j < samples; ++j)
-    {
-      Eigen::MatrixXf output = input.row(j);
-      for (GPULayer *layer : layers)
-        output = layer->forwardPropagation(output);
-
-      result.push_back(output);
-    }
-
-    return result;
-  }
-
-  // train the network
-  virtual void fit(Eigen::MatrixXf x_train, Eigen::MatrixXf y_train, int epochs, float learningRate)
-  {
-    int samples = x_train.rows();
-    std::cout << "Samples: " << samples << std::endl;
-    printMatrixSize("x_train", x_train);
-    printMatrixSize("y_train", y_train);
-
-    std::vector<int> order(samples);
-    std::iota(order.begin(), order.end(), 0);
-
-    // training loop
-    for (int i = 0; i < epochs; ++i)
-    {
-      float err = 0.0f;
-
-      // feed forward
-      std::random_shuffle(order.begin(), order.end());
-
-      // forward propagation
-      for (int j = 0; j < samples; ++j)
-      {
-        int index = order[j];
-        Eigen::MatrixXf output = x_train.row(index);
-
-        for (GPULayer *layer : layers)
-          output = layer->forwardPropagation(output);
-
-        // compute loss(for display purpose only)
-        Eigen::MatrixXf y = y_train.row(index);
-
-        err += loss(y, output);
-
-        // backward propagation
-        Eigen::MatrixXf error = lossPrime(y, output);
-
-        for (std::vector<GPULayer *>::reverse_iterator layer = layers.rbegin(); layer != layers.rend(); ++layer)
-          error = (*layer)->backwardPropagation(error, learningRate);
-      }
-      err /= (float)samples;
-      std::cout << "Epoch " << (i + 1) << "/" << epochs << " error = " << err << std::endl;
-    }
-  }
-
-protected:
-  std::vector<GPULayer *> layers;
-  std::function<float(Eigen::MatrixXf &, Eigen::MatrixXf &)> loss;
-  std::function<Eigen::MatrixXf(Eigen::MatrixXf &, Eigen::MatrixXf &)> lossPrime;
-};
-#endif
+	for (int i = 0; i < 3; i++)
+		timeAndStoreOperations("gpu_xor.txt");
+	return 0;
+}
---
+++ RealGPUNeuralNetwork.h
@@ -0,0 +1,196 @@
+#ifndef NEURAL_NET_INC
+#define NEURAL_NET_INC
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <Eigen/Dense>
+#include <numeric> //std::iota
+
+extern Eigen::MatrixXf HostMatrixMultiply(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
+extern Eigen::MatrixXf HostMatrixScalarMultiply(const Eigen::MatrixXf &M, float scalar);
+extern Eigen::MatrixXf HostMatrixAddition(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
+extern Eigen::MatrixXf HostMatrixSubtraction(const Eigen::MatrixXf &M, const Eigen::MatrixXf &N);
+
+void printMatrixSize(const std::string msg, const Eigen::MatrixXf &m)
+{
+  std::cout << msg.c_str() << "[" << m.rows() << "," << m.cols() << "]" << std::endl;
+}
+
+class GPULayer
+{
+public:
+  GPULayer() : input(), output() {}
+  virtual ~GPULayer() {}
+
+  virtual Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf &input) = 0;
+  virtual Eigen::MatrixXf backwardPropagation(Eigen::MatrixXf &output, float learningRate) = 0;
+
+protected:
+  Eigen::MatrixXf input;
+  Eigen::MatrixXf output;
+};
+
+class GPUDenseLayer : public GPULayer
+{
+public:
+  GPUDenseLayer(int inputSize, int outputSize)
+  {
+    weights = HostMatrixScalarMultiply(Eigen::MatrixXf::Random(inputSize, outputSize), 0.5f);
+    bias = HostMatrixScalarMultiply(Eigen::MatrixXf::Random(1, outputSize), 0.5f);
+  }
+
+  Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf &input)
+  {
+    this->input = input;
+    this->output = HostMatrixAddition(HostMatrixMultiply(input, weights), bias);
+    return this->output;
+  }
+
+  
+  Eigen::MatrixXf backwardPropagation(Eigen::MatrixXf &outputError, float learningRate)
+  {
+    Eigen::MatrixXf inputError = HostMatrixMultiply(outputError, weights.transpose());
+    Eigen::MatrixXf weightsError = HostMatrixMultiply(input.transpose(), outputError);
+
+    weights = HostMatrixSubtraction(weights, HostMatrixScalarMultiply(weightsError, learningRate));
+    bias = HostMatrixSubtraction(bias, HostMatrixScalarMultiply(outputError, learningRate));
+
+    return inputError;
+  }
+
+private:
+  Eigen::MatrixXf weights;
+  Eigen::MatrixXf bias;
+};
+
+class GPUActivationLayer : public GPULayer
+{
+public:
+  GPUActivationLayer(std::function<float(float)> activation,
+                     std::function<float(float)> activationPrime)
+  {
+    this->activation = activation;
+    this->activationPrime = activationPrime;
+  }
+
+  Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf &input)
+  {
+    this->input = input;
+    this->output = input.unaryExpr(activation);
+    return this->output;
+  }
+
+  Eigen::MatrixXf backwardPropagation(Eigen::MatrixXf &outputError, float learningRate)
+  {
+    return (input.unaryExpr(activationPrime).array() * outputError.array()).matrix();
+  }
+
+private:
+  std::function<float(float)> activation;
+  std::function<float(float)> activationPrime;
+};
+
+class GPUFlattenLayer : public GPULayer
+{
+public:
+  Eigen::MatrixXf forwardPropagation(Eigen::MatrixXf &input)
+  {
+    this->input = input;
+    this->output = input;
+    this->output.resize(1, input.rows() * input.cols()); // flatten
+    return this->output;
+  }
+  Eigen::MatrixXf backwardPropagation(Eigen::MatrixXf &outputError, float learningRate)
+  {
+    outputError.resize(input.rows(), input.cols());
+    return outputError;
+  }
+};
+
+class GPUNetwork
+{
+public:
+  GPUNetwork() {}
+  virtual ~GPUNetwork() {}
+
+  void add(GPULayer *layer)
+  {
+    layers.push_back(layer);
+  }
+
+  void use(std::function<float(Eigen::MatrixXf &, Eigen::MatrixXf &)> lossF, std::function<Eigen::MatrixXf(Eigen::MatrixXf &, Eigen::MatrixXf &)> lossDer)
+  {
+    loss = lossF;
+    lossPrime = lossDer;
+  }
+
+  std::vector<Eigen::MatrixXf> predict(Eigen::MatrixXf input)
+  {
+    int samples = input.rows();
+
+    std::vector<Eigen::MatrixXf> result;
+
+    // forward propagation
+    for (int j = 0; j < samples; ++j)
+    {
+      Eigen::MatrixXf output = input.row(j);
+      for (GPULayer *layer : layers)
+        output = layer->forwardPropagation(output);
+
+      result.push_back(output);
+    }
+
+    return result;
+  }
+
+  // train the network
+  virtual void fit(Eigen::MatrixXf x_train, Eigen::MatrixXf y_train, int epochs, float learningRate)
+  {
+    int samples = x_train.rows();
+    std::cout << "Samples: " << samples << std::endl;
+    printMatrixSize("x_train", x_train);
+    printMatrixSize("y_train", y_train);
+
+    std::vector<int> order(samples);
+    std::iota(order.begin(), order.end(), 0);
+
+    // training loop
+    for (int i = 0; i < epochs; ++i)
+    {
+      float err = 0.0f;
+
+      // feed forward
+      std::random_shuffle(order.begin(), order.end());
+
+      // forward propagation
+      for (int j = 0; j < samples; ++j)
+      {
+        int index = order[j];
+        Eigen::MatrixXf output = x_train.row(index);
+
+        for (GPULayer *layer : layers)
+          output = layer->forwardPropagation(output);
+
+        // compute loss(for display purpose only)
+        Eigen::MatrixXf y = y_train.row(index);
+
+        err += loss(y, output);
+
+        // backward propagation
+        Eigen::MatrixXf error = lossPrime(y, output);
+
+        for (std::vector<GPULayer *>::reverse_iterator layer = layers.rbegin(); layer != layers.rend(); ++layer)
+          error = (*layer)->backwardPropagation(error, learningRate);
+      }
+      err /= (float)samples;
+      std::cout << "Epoch " << (i + 1) << "/" << epochs << " error = " << err << std::endl;
+    }
+  }
+
+protected:
+  std::vector<GPULayer *> layers;
+  std::function<float(Eigen::MatrixXf &, Eigen::MatrixXf &)> loss;
+  std::function<Eigen::MatrixXf(Eigen::MatrixXf &, Eigen::MatrixXf &)> lossPrime;
+};
+#endif
